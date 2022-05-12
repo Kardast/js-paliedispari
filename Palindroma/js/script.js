33f03a22a811bf339cc3b6644af07a6b27eececb
Kardast <dottorsf@gmail.com>
2022-05-12 17:57:44 +0200

    Palindroma with cycle

--- Palindroma/js/script.js
+++ Palindroma/js/script.js
@@ -3,6 +3,28 @@
 // Creare una funzione per capire se la parola inserita è palindroma
 
 
+// let myWord = prompt("Inserisci una parola");
+// console.log(myWord);
+
+// let palindromaResult = palindroma(myWord);
+// console.log(palindromaResult);
+
+// function palindroma(wordCheck) {
+//     // funzione che controlla se la parola è palindroma o no
+
+//     // prendo la parola, la divido in singole lettere minuscole, la inverto e la converto in stringa
+//     let myWordChecking = myWord.toLowerCase().split("").reverse().toString();
+//     console.log(typeof myWordChecking);
+//     console.log(typeof myWord);
+
+//     // con questa operazione condizionale controllo se la parola inserita (anch'essa divisa in singole lettere minuscole e convertita in stringa) è uguale alla variante creata sopra
+//     if (myWordChecking === myWord.toLowerCase().split("").toString()) {
+//         return "La parola è palindroma.";
+//     } else {
+//         return "La parola non è palindroma.";
+//     }
+// }
+
 let myWord = prompt("Inserisci una parola");
 console.log(myWord);
 
@@ -12,13 +34,22 @@ console.log(palindromaResult);
 function palindroma(wordCheck) {
     // funzione che controlla se la parola è palindroma o no
 
-    // prendo la parola, la divido in singole lettere minuscole, la inverto e la converto in stringa
-    let myWordChecking = myWord.toLowerCase().split("").reverse().toString();
-    console.log(typeof myWordChecking);
+    // creazione di contenitore per la parola inversa
+    let holder = "";
+    // creazione di variabile che guarda la sua lunghezza -1
+    let wordLen = wordCheck.length-1;
+    // creazione di ciclo con i uguale alla lunghezza della parola e a ogni giro diminuisce di uno
+    for (let i = wordLen; i >= 0; i--) {
+        // assegnazione della lettera [i] al contenitore, dove [i] è la lettera alla posizione della lunghezza della stessa -1
+        holder += wordCheck[i];
+    }
+
+    holder.toLowerCase().toString();
+    console.log(typeof holder);
     console.log(typeof myWord);
 
-    // con questa operazione condizionale controllo se la parola inserita (anch'essa divisa in singole lettere minuscole e convertita in stringa) è uguale alla variante creata sopra
-    if (myWordChecking === myWord.toLowerCase().split("").toString()) {
+
+    if (holder === myWord.toLowerCase().toString()) {
         return "La parola è palindroma.";
     } else {
         return "La parola non è palindroma.";
